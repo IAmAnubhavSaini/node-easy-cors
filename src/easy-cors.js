27b89e2ffd66c2de5e602d5c5338ccd908cb3b0e
Anubhav <iamanubhavsaini@gmail.com>
2021-01-22 11:40:46 +0530

    Update

--- src/easy-cors.js
+++ src/easy-cors.js
@@ -1,32 +1,31 @@
 const assert = require('assert')
 
 /**
- * 
+ *
  * @param {Request} req Express request object
  * @param {Response} res Express Response object
  * @param {Function} next Express next function
  */
 function middleware(req, res, next) {
-    assert.equal(typeof next, 'function', 'next should be a function')
+    assert.strictEqual(typeof next, 'function', 'next should be a function')
     assert.ok(res.send, 'Response should have a send function')
     assert.ok(res.header, 'Response should have a header function')
     assert.ok(req.headers, 'Request should have a headers object')
-    assert.notEqual(req.headers.origin, undefined, 'Request should have a headers.origin value')
+    assert.notStrictEqual(req.headers['origin'], undefined, 'Request should have a headers.origin value')
     res.header("Access-Control-Allow-Credentials", true)
-    res.header("Access-Control-Allow-Origin", req.headers.origin)
+    res.header("Access-Control-Allow-Origin", req.headers['origin'])
     res.header("Access-Control-Allow-Headers", (req.headers['access-control-request-headers']) || "Content-Type,X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5,  Date, X-Api-Version, X-File-Name")
     res.header("Access-Control-Allow-Methods", (req.headers['access-control-request-method']) || "POST, GET, PUT, DELETE, OPTIONS")
-    if ('OPTIONS' == req.method) {
+    if ('OPTIONS' === req.method) {
         res.send(200)
-    }
-    else {
+    } else {
         next()
     }
 }
 
 /**
  * Calls the easyCors middleware function for all the requests. Also the example of the usage for middleware function.
- * @param {express app} app 
+ * @param {express app} app
  */
 function easyCors(app) {
     assert.ok(app.all, 'app should have all function')
